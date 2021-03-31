1c05c23db041dc2917b248203c49068113a00ba5
sebastiano <sebainsin@gmail.com>
2021-03-31 15:54:05 +0200

    new feature

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
 #include "BitmapImage.h"
-
+#include "version.h"
 int main() {
-    std::cout << "Image viewer" << std::endl;
+    std::cout << "Image viewer-v." << std::endl;
     BitmapImage b;
     std::string imgName = "test.png";
     if (b.load(imgName)) {
-
-
         std::cout << "show image: " << imgName << std::endl;
         b.save("new.test.jpg");
     }
---
+++ version.h
@@ -0,0 +1,8 @@
+//
+// Created by sebastiano on 31/03/21.
+//
+
+#ifndef MAIN_CPP_VERSION_H
+#define MAIN_CPP_VERSION_H
+const std::string VERSION="1.0";
+#endif //MAIN_CPP_VERSION_H
