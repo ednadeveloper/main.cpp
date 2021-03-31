9d05a65bbde62c1531ab8a42385dcae771c8881e
sebastiano <sebainsin@gmail.com>
2021-03-31 15:28:06 +0200

    *noice

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@ int main() {
     std::cout << "Image viewer" << std::endl;
     BitmapImage b;
     std::string imgName = "test.png";
-    if (b.load(imgName))
+    if (b.load(imgName)) {
+
+
         std::cout << "show image: " << imgName << std::endl;
-    else
+        b.save("new.test.jpg");
+    }
+        else
         std::cerr << "image:" << imgName << " not readable." << std::endl;
     return 0;
 }
